e535d4c8b8f64025fe83b6941ee4bba37c35fe1d
Tiago Xavier <tiagolxavier@gmail.com>
2024-03-01 17:26:19 -0300

    package fix

--- node/server.js
+++ node/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require("body-parser")
 const app = express()
 const port = 3000
 
@@ -20,6 +21,9 @@ app.engine('handlebars', expressHandlebars.engine({
 }));
 
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', async function (req, res) {
 
